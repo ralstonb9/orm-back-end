89f8549a7751a740cb36dfb76978d656007da85f
ralstonb9 <ralstonb9@gmail.com>
2023-09-12 21:49:48 -0700

    added tag-routes and fixed minor mistake in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(
       req.params.id, 
-      {include: [{model: Category}, {model: Tag}],
+      {include: [{model: Category}, {model: Tag}]
     });
     res.status(200).json(productData)
   } catch (err) {
